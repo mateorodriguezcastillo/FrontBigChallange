ee3feaae8dcbb0f45a421606d41d33422d259cdd
mateorodriguezcastillo <mateo.rodriguez@lightit.io>
2023-01-06 12:51:42 -0300

    feat: Login and Register Pages

--- components/ui/TextInput.tsx
+++ components/ui/TextInput.tsx
@@ -4,27 +4,37 @@ import { upperFirst } from "lodash/fp";
 
 interface TextInputProps {
   inputName: string;
-  className?: string;
+  inputClassName?: string;
+  labelClassName?: string;
+  type: string;
 }
 
-export const TextInput: FC<TextInputProps> = ({ inputName, className }) => {
+export const TextInput: FC<TextInputProps> = ({
+  inputName,
+  inputClassName,
+  labelClassName,
+  type,
+}) => {
   return (
     <div className={"flex flex-grow flex-col"}>
       <label
         htmlFor={inputName}
-        className="mb-2 block text-sm text-gray-800 dark:text-white"
+        className={tw(
+          "mb-2 block text-sm text-gray-800 dark:text-white",
+          labelClassName
+        )}
       >
         {upperFirst(inputName)}
       </label>
       <input
-        type="text"
+        type={type}
         id={inputName}
         className={tw(
           `mb-6 mr-2 block w-1/2 rounded-lg border 
                             border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 
                             focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 
                             dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500`,
-          className
+          inputClassName
         )}
       />
     </div>
---
+++ pages/auth/login.tsx
@@ -0,0 +1,32 @@
+import { Button } from "flowbite-react";
+import { UserIcon } from "../../components/icons";
+import { AuthLayout } from "../../components/layout";
+import { TextInput } from "../../components/ui";
+
+const LoginPage = () => {
+  return (
+    <AuthLayout
+      title={"Sign In"}
+      pageDescription={"Enter requested data to sign in"}
+    >
+      <div className="mt-16 mr-4 ml-4 flex flex-col">
+        <UserIcon className="m-auto h-24 w-24 text-white" />
+        <TextInput
+          inputClassName="w-full bg-white border border-gray-300 shadow-xl"
+          labelClassName="text-white"
+          type="text"
+          inputName={"email"}
+        />
+        <TextInput
+          inputClassName="w-full bg-white border border-gray-300 shadow-xl"
+          labelClassName="text-white"
+          type="password"
+          inputName={"password"}
+        />
+        <Button className="mt-4 w-full shadow-xl">Sign In</Button>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default LoginPage;
