a08f2370df261e935114906e23ad267e4c98b732
mateorodriguezcastillo <mateo.rodriguez@lightit.io>
2023-02-07 16:18:26 -0300

    fix: remove useless class

--- components/ui/TextInput.tsx
+++ components/ui/TextInput.tsx
@@ -59,7 +59,7 @@ export const TextInput: FC<TextInputProps> = ({
           type={currentType}
           id={inputName}
           className={tw(
-            `!autofill:bg-red-500 !autofill:text-white mr-2 block w-1/2 rounded-lg border
+            `mr-2 block w-1/2 rounded-lg border
                             border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 
                             focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 
                             dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500`,
