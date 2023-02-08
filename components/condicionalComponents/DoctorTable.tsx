ef8d56e9147b2d09aa7321cfa2812e42a9244a47
mateorodriguezcastillo <115570361+mateorodriguezcastillo@users.noreply.github.com>
2023-02-08 13:35:01 -0300

    feat: doctor private pool (#11)
    * feat: doctor private pool
    
    * fixes and add uuidv4

--- components/condicionalComponents/DoctorTable.tsx
+++ components/condicionalComponents/DoctorTable.tsx
@@ -1,55 +1,42 @@
-import axios from "axios";
 import { useState } from "react";
-import { useQuery } from "react-query";
 import { Status } from "../../interfaces";
 import { useAuthStore } from "../../src/store/auth";
 import { SubmissionsTable } from "../submissions";
 import { NoContent } from "../ui";
-
-const getData = async (
-  currentPage: number,
-  status: Status | "",
-  token: string
-) => {
-  const res = await axios.get(
-    "http://localhost/api/submission?page=" + currentPage + "&status=" + status,
-    {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    }
-  );
-  return res.data;
-};
+import { useDoctorSubmissionsQuery } from "../../hooks/useDoctorSubmissionsQuery";
 
 export const DoctorTable = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [status, setStatus] = useState<Status | "">("");
-  const { user, token } = useAuthStore();
+  const [viewOwnSubmissions, setViewOwnSubmissions] = useState(false);
+  const { user } = useAuthStore();
 
-  const { data: allSubmissions, isLoading: loadingSubmissions } = useQuery(
-    ["submissions", currentPage, status, token],
-    () => getData(currentPage, status, token),
-    { enabled: user?.role_name === "doctor" }
-  );
+  const { submissions, pagination, allSubmissions, ownSubmissions } =
+    useDoctorSubmissionsQuery(
+      user?.id,
+      viewOwnSubmissions,
+      currentPage,
+      status
+    );
+
   return (
     <>
-      {!loadingSubmissions &&
-      allSubmissions &&
-      allSubmissions.data.length > 0 ? (
+      {!allSubmissions.isLoading &&
+      !ownSubmissions.isLoading &&
+      submissions.length > 0 ? (
         <SubmissionsTable
-          submissions={allSubmissions.data}
-          pagination={allSubmissions.pagination}
+          submissions={submissions}
+          pagination={pagination}
           status={status}
+          viewOwnSubmissions={viewOwnSubmissions}
           changeStatus={setStatus}
           changePage={setCurrentPage}
+          changeViewOwnSubmissions={setViewOwnSubmissions}
         />
       ) : (
-        !loadingSubmissions &&
-        allSubmissions &&
-        allSubmissions.data.length === 0 && (
-          <NoContent contentType="submissions" />
-        )
+        !allSubmissions.isLoading &&
+        !ownSubmissions.isLoading &&
+        submissions.length === 0 && <NoContent contentType="submissions" />
       )}
     </>
   );
---
+++ components/ui/Switch.tsx
@@ -0,0 +1,47 @@
+import { FC } from "react";
+import { tw } from "../../utils/merge-classnames";
+import { uuid } from "uuidv4";
+
+interface SwitchProps {
+  className?: string;
+  checked: boolean;
+  label?: string;
+  onChange: (checked: boolean) => void;
+}
+
+export const Switch: FC<SwitchProps> = ({
+  className,
+  checked,
+  label,
+  onChange,
+}) => {
+  const checkboxId = uuid();
+
+  return (
+    <div className={tw("flex items-center", className)}>
+      <label
+        htmlFor={checkboxId}
+        className={tw(
+          "relative h-7 w-14 cursor-pointer rounded-full bg-gray-200 dark:bg-gray-600",
+          className
+        )}
+      >
+        <input
+          type="checkbox"
+          id={checkboxId}
+          className="peer sr-only"
+          defaultChecked={checked}
+          onChange={() => {
+            onChange(!checked);
+          }}
+        />
+        <span className="absolute left-[3px] top-[3px] h-4/5 w-2/5 rounded-full bg-gray-400 transition-all duration-500 peer-checked:left-[30.5px] peer-checked:bg-blue-600 dark:peer-checked:bg-white" />
+      </label>
+      {label && (
+        <span className="ml-2 text-sm text-gray-800 dark:text-white">
+          {label}
+        </span>
+      )}
+    </div>
+  );
+};
